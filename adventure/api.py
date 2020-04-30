42ddedf5fa7dd375e676f102d27075c03a8f99be
Dorothy Gasque <dggasque@gmail.com>
2020-04-30 16:20:29 -0700

    Update api.py

--- adventure/api.py
+++ adventure/api.py
@@ -11,7 +11,7 @@ import json
 # instantiate pusher
 # pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
 
-@csrf_exempt
+#@csrf_exempt
 @api_view(["GET"])
 def initialize(request):
     user = request.user
@@ -22,7 +22,7 @@ def initialize(request):
     players = room.playerNames(player_id)
     return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
 
-@csrf_exempt
+#@csrf_exempt
 @api_view(["GET"])
 def get_world(request):
     rooms = Room.objects.all()
@@ -71,7 +71,7 @@ def move(request):
 
 
 
-@csrf_exempt
+#@csrf_exempt
 @api_view(["POST"])
 def say(request):
     # IMPLEMENT
