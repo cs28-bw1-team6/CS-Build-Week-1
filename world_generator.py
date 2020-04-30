f92f23f9d4d7830f87df6b5e4ff72d47b189661d
Dorothy Gasque <dggasque@gmail.com>
2020-04-30 16:09:01 -0700

    update world_generator.py in main directory and removes extra world_generator.py file from the util directory

--- world_generator.py
+++ world_generator.py
@@ -4,34 +4,6 @@ from django.contrib.auth.models import User
 from adventure.models import Player, Room
 
 Room.objects.all().delete()
-# class Room:
-#     def __init__(self, id, name, description, x, y):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.n_to = None
-#         self.s_to = None
-#         self.e_to = None
-#         self.w_to = None
-#         self.x = x
-#         self.y = y
-#     def __repr__(self):
-#         if self.e_to is not None:
-#             return f"({self.x}, {self.y}) -> ({self.e_to.x}, {self.e_to.y})"
-#         return f"({self.x}, {self.y})"
-#     def connect_rooms(self, connecting_room, direction):
-#         '''
-#         Connect two rooms in the given n/s/e/w direction
-#         '''
-#         reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
-#         reverse_dir = reverse_dirs[direction]
-#         setattr(self, f"{direction}_to", connecting_room)
-#         setattr(connecting_room, f"{reverse_dir}_to", self)
-#     def get_room_in_direction(self, direction):
-#         '''
-#         Connect two rooms in the given n/s/e/w direction
-#         '''
-#         return getattr(self, f"{direction}_to")
 
 class World:
     def __init__(self):
@@ -50,6 +22,7 @@ class World:
         for i in range( len(self.grid) ):
             self.grid[i] = [None] * size_x
             
+        # Randomizes room order
         shuffled_rooms = list(rooms.keys())
         shuffle(shuffled_rooms)
     
@@ -69,43 +42,47 @@ class World:
 
             # Calculate the direction of the room to be created
             if direction > 0 and x < size_x - 1:
-                room_direction = "e"
                 x += 1
             elif direction < 0 and x > 0:
-                room_direction = "w"
                 x -= 1
             else:
                 # If we hit a wall, turn north and reverse direction
-                room_direction = "n"
                 y += 1
                 direction *= -1
 
-            # Create a room in the given direction
-            # room = Room(room_count, shuffled_keys[room_count] , r_general[shuffled_keys[room_count]], x, y)
-            # Note that in Django, you'll need to save the room after you create it
-            # room.save()
+            # Initializes first room of the world.
             if room_count == 0:
                 room = Room(room_id=room_count, title="Foyer", description="As you enter the castle, the door slams shut locking you in. Can you find your way out?", x=x, y=y)
+            # Creates room objects using the rooms dictionary.
             else:
                 room = Room(room_id=room_count, title=shuffled_rooms[room_count-1], description=rooms[shuffled_rooms[room_count-1]], x=x, y=y)
             # Save the room in the World grid
             self.grid[y][x] = room
+            # Save room object in the database
             room.save()
+            # Increae room count to be used to index both the room key list and to assign room id
             room_count += 1
 
+        # Connect the rooms using a technique common to maze generation
+        # Visited array will keep track of rooms that our walker has visited
+
         vis = [[0] * self.width for _ in range(self.height)] 
-        
-            
+
+        # walk is a recursive function used to connect the rooms randomly with a "walker"   
         def walk(x, y):
+            # Setting the value of the visited array element as 1 indicates a room has been visited.
             vis[y][x] = 1
+            # Reverse direction dictionary is used to make sure rooms are connected in both directions 
             reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
-            
+            # d is an array of neighboring rooms 
             d = [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]
+            # shuffling allows for randomization of the stack as the function is called recursively
             shuffle(d)
+            # Create connections between visited rooms and their neighbors
             for (xx, yy) in d:
+                # Check to make sure the array indexes are within the range of self.grid
                 if xx in range(self.width) and yy in range(self.height): 
                     if vis[yy][xx]: continue
-                #     if xx == x: hor[max(y, yy)][x] = "+  "
                     if xx == x :
                         if yy < y:
                             room_direction = "s"
@@ -117,7 +94,6 @@ class World:
                             reverse_dir = reverse_dirs[room_direction]
                             self.grid[y][x].connectRooms(self.grid[yy][xx], room_direction)
                             self.grid[yy][xx].connectRooms(self.grid[y][x], reverse_dir)
-                #     if yy == y: ver[y][max(x, xx)] = "   "
                     if yy == y:
                         if xx < x:
                             room_direction = "w"
@@ -131,74 +107,24 @@ class World:
                             self.grid[yy][xx].connectRooms(self.grid[y][x], reverse_dir)
                     walk(xx, yy)
 
+        # Select random indexes to begin the recursive walk function
         start_x = randrange(self.width)
         start_y = randrange(self.height)
         
         walk(start_x, start_y)
 
 
-    # def print_rooms(self):
-    #     '''
-    #     Print the rooms in room_grid in ascii characters.
-    #     '''
+   
 
-    #     # Add top border
-    #     str = "# " * ((3 + self.width * 5) // 2) + "\n"
-
-    #     # The console prints top to bottom but our array is arranged
-    #     # bottom to top.
-    #     #
-    #     # We reverse it so it draws in the right direction.
-    #     reverse_grid = list(self.grid) # make a copy of the list
-    #     reverse_grid.reverse()
-    #     for row in reverse_grid:
-    #         # PRINT NORTH CONNECTION ROW
-    #         str += "#"
-    #         for room in row:
-    #             if room is not None and room.n_to is not None:
-    #                 str += "  |  "
-    #             else:
-    #                 str += "     "
-    #         str += "#\n"
-    #         # PRINT ROOM ROW
-    #         str += "#"
-    #         for room in row:
-    #             if room is not None and room.w_to is not None:
-    #                 str += "-"
-    #             else:
-    #                 str += " "
-    #             if room is not None:
-    #                 str += f"{room.id}".zfill(3)
-    #             else:
-    #                 str += "   "
-    #             if room is not None and room.e_to is not None:
-    #                 str += "-"
-    #             else:
-    #                 str += " "
-    #         str += "#\n"
-    #         # PRINT SOUTH CONNECTION ROW
-    #         str += "#"
-    #         for room in row:
-    #             if room is not None and room.s_to is not None:
-    #                 str += "  |  "
-    #             else:
-    #                 str += "     "
-    #         str += "#\n"
-
-    #     # Add bottom border
-    #     str += "# " * ((3 + self.width * 5) // 2) + "\n"
-
-    #     # Print string
-    #     print(str)
-
-
+# Create world object instance
 w = World()
 num_rooms = 100
 width = 10
 height = 10
+# Generate rooms
 w.generate_rooms(width, height, num_rooms)
-# w.print_rooms()
 
+#Initialize all players to begin at the 
 players=Player.objects.all()
 for p in players:
   p.currentRoom=0
